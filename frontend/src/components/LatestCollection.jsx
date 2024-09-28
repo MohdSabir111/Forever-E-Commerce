239c3b2f6d0bad1ce1cc95ed81f5888cc51cb4b9
MOHD SABIR <sabiransari01981@gmail.com>
2024-09-28 15:21:57 +0530

    creating frontEnd part---Navbar Done----

---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { ShopContext } from './../context/ShopContext';
+import Title from './Title';
+import ProductItem from './ProductItem';
+
+
+function LatestCollection() {
+ 
+    const { products } = useContext(ShopContext);
+    const [latestProduct, setLatestProdeuct]=useState([]);
+
+    useEffect(()=>{
+        
+        setLatestProdeuct(products.slice(0,10))
+    },[])
+            
+  return (
+    <div className='my-10' >
+         <div className='text-center text-3xl py-8' >
+            <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600 '>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat debitis ullam error inventore delectus. Illo delectus asperiores atque quaerat ducimus beatae natus ipsa nulla iure hic, corrupti, sed nobis. Tempore!
+             </p>
+         </div>
+        {/** Rendering products  */}
+            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6 ' >
+                    {
+                        latestProduct.map((item,index)=>(
+                            <ProductItem key={index} id={item._id} image={item.image} name={item.name} price={item.price} />
+                        ))
+                    }
+            </div>
+
+    </div>
+  )
+}
+
+export default LatestCollection
