af12f684345b6f18b8ceaecc62a284b842a6ebe0
MOHD SABIR <sabiransari01981@gmail.com>
2024-09-29 10:27:25 +0530

    --NewsLatterBox UI Done--

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
 import OurPolicy from '../components/OurPolicy'
+import NewsLatterBox from '../components/NewsLatterBox'
 
 function Home() {
   return (
@@ -11,6 +12,7 @@ function Home() {
        <LatestCollection/>
        <BestSeller/>
        <OurPolicy/>
+       <NewsLatterBox/>
     </div>
   )
 }
