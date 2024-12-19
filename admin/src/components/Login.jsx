b54cfc5f29d9c0a153898a40def1b988bd99a2be
MOHD SABIR <sabiransari01981@gmail.com>
2024-12-19 17:03:20 +0530

    Login and Logout logic is done

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../App';
+import { toast } from 'react-toastify';
 
-function Login() {
+function Login({setToken}) {
+
+    const [email, setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    
+ 
+   const onSubmitHandler =async (e) => {
+     try {
+        e.preventDefault();
+       const response = await axios.post(backendUrl + '/api/user/admin',{email, password});
+       console.log(response)
+       if(response.data.success){
+        setToken(response.data.token)
+       }else{
+        toast.error(response.data.message)
+       }
+        
+     } catch (error) {
+        toast.error(response.data.message)
+        console.log(error)
+     }
+   }
+
   return (
-    <div>
+    <div className='flex items-center justify-center min-h-screen w-full ' >
+         <div className='bg-white rounded-lg shadow-md max-w-md px-8 py-6'>
+            <h1 className='text-2xl font-bold mb-4'>Admin Panel</h1>
+
+            <form onSubmit={onSubmitHandler} >
+                <div className='min-w-72 mb-2'>
+                    <p className='font-medium text-sm text-gay-700 mb-2 '>Email Address</p>
+                    <input onChange={(e)=>setEmail(e.target.value)}  value={email} className='rounded-md w-full  px-3 py-2 border border-gray-300  outline-none'  type="email" placeholder='Enter Your Email ' required />
+                </div>
+                <div className='min-w-72 mb-2'>
+                    <p className='font-medium text-sm text-gay-700 mb-2 ' >Password</p>
+                    <input onChange={(e)=>setPassword(e.target.value)}  value={password} className='rounded-md w-full  px-3 py-2 border border-gray-300  outline-none' type="password" placeholder='Enter Your Password ' required />
+                </div>
+                <button type='submit' className='w-full mt-2 bg-black text-white px-4 py-2 rounded-md'>Login</button>
+            </form>
+
+         </div>
       
     </div>
   )
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets'
-function Navbar() {
+function Navbar({setToken}) {
   return (
     <div className='flex items-center justify-between px-[4%] py-2'>
       <img className='w-[max(10%,80px)] ' src={assets.logo} alt="" /> 
-      <button className='text-white bg-gray-600 px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm '>Logout</button>
+      <button onClick={()=>setToken('')} className='text-white bg-gray-600 px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm '>Logout</button>
     </div>
   )
 }
